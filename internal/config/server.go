67b49edb004bace4f1fc5967029037f2a53f0c22
Andrei Suslov <Cyclov@list.ru>
2026-08-18 12:38:07 +0300

    bug fix

--- internal/config/server.go
+++ internal/config/server.go
@@ -16,16 +16,22 @@ type ServerSettings struct {
 }
 
 func ServerConfig() (ServerSettings, error) {
-	var cfg ServerSettings
-	flag.StringVar(&cfg.SrvAdr, "a", ":8080", "HTTP server address")
-	flag.DurationVar(&cfg.StoreInterval, "i", 300*time.Second, "metrics store interval")
-	flag.StringVar(&cfg.FileStoragePath, "f", "metrics.json", "metrics storage file path")
-	flag.BoolVar(&cfg.Restore, "r", true, "restore metrics from storage file")
+	var settings ServerSettings
+
+	srvAdr := flag.String("a", ":8080", "HTTP server address")
+	storeInterval := flag.Int64("i", 300, "metrics store interval in seconds")
+	fileStoragePath := flag.String("f", "metrics.json", "metrics storage file path")
+	restore := flag.Bool("r", true, "restore metrics from storage file")
 	flag.Parse()
 
-	if err := env.Parse(&cfg); err != nil {
+	settings.SrvAdr = *srvAdr
+	settings.StoreInterval = time.Duration(*storeInterval) * time.Second
+	settings.FileStoragePath = *fileStoragePath
+	settings.Restore = *restore
+
+	if err := env.Parse(&settings); err != nil {
 		return ServerSettings{}, fmt.Errorf("parse env config: %w", err)
 	}
 
-	return cfg, nil
+	return settings, nil
 }
